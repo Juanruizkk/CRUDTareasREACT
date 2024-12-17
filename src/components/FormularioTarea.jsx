f0c360b12362d3c213114debafa4912c12d7d604
juanruizkk <juanignacioruizr@gmail.com>
2024-12-17 12:15:32 -0500

    Agregamos las tareas validadas al estado listaTareas

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,13 +1,45 @@
+import React, { useState } from "react"; // Agrega useState aquí
 import { Button } from "react-bootstrap";
 import { Form } from "react-bootstrap";
 import { PlusCircle } from "react-bootstrap-icons";
-import ListaTareas from "./ListaTareas"
+import ListaTareas from "./ListaTareas";
+import { useForm } from "react-hook-form";
+
 const FormularioTarea = () => {
+
+  const { register, handleSubmit, formState: { errors }, reset } = useForm();
+  /* la lista de tareas es un state */
+  const [listaTareas, setListaTareas] = useState([]);
+
+
+  const posteriorValidacion = (data) => {
+    if (listaTareas.some((tarea) => tarea === data.tarea.trim())) {
+      alert('Esta tarea ya existe');
+    } else {
+      /* ... operador spread */
+      setListaTareas([...listaTareas, data.tarea]);
+      reset();
+    }
+
+  }
+
+
   return (
     <section className="mb-4">
-      <Form Form className="container d-flex" >
+      <Form onSubmit={handleSubmit(posteriorValidacion)} className="container d-flex" >
         <Form.Group controlId="formTarea" className="w-100">
-          <Form.Control type="text" placeholder="Ingrese una tarea" />
+          <Form.Control type="text" placeholder="Ingrese una tarea" {...register("tarea", {
+            required: 'La tarea es un dato obligatorio',
+            minLength: {
+              value: 3,
+              message: 'La tarea debe tener 3 carateres como minimo'
+            },
+            maxLength: {
+              value: 25,
+              message: 'La tarea debe tener como maximo 25 caracteres'
+            }
+          })} />
+          <Form.Text className="text-danger">{errors.tarea?.message}</Form.Text>
         </Form.Group>
         <Button variant="primary" type="submit">
           <PlusCircle></PlusCircle>
